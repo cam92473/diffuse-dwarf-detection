folder for psfs, fixed r999s, working on completion and artificial formula.

ARTIFICIAL/artificial/create_convolve_dwarfs.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import argparse
 import time
 
-def create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,y0s,num_dwarfs,r99s,psfkernel,convolve,verbose):
+def create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,y0s,num_dwarfs,r99s,psfkernel,convolve,diagnostic_images,verbose):
 
     thetas_rad = radians(thetas+90)
     ellipticities = 1 - axisratios
@@ -52,8 +52,10 @@ def create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,
         t2 = time.perf_counter()
         print(f"tray filling time: {t2-t1}")
 
-    #plt.imshow(tray)
-    #plt.show()
+    if diagnostic_images:
+        plt.imshow(np.log10(tray+1))
+        plt.title("log image of sticker-filled tray")
+        plt.show()
 
     if convolve:
         if verbose:
@@ -67,8 +69,10 @@ def create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,
         print("note: not convolving")
         conv_tray = tray
     
-    #plt.imshow(conv_tray)
-    #plt.show()
+    if diagnostic_images:
+        plt.imshow(np.log10(conv_tray+1))
+        plt.title("log image of convolved tray")
+        plt.show()
 
     if verbose:
         print("adding tray to image")
@@ -78,8 +82,14 @@ def create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,
         t2 = time.perf_counter()
         print(f"adding tray time: {t2-t1}")
 
-    #plt.imshow(data)
-    #plt.show()
+    if diagnostic_images:
+        fig, ax = plt.subplots()
+        logdata = np.log10(data-data.min()+1)
+        #the vmin and vmax parameters here are arbitrary
+        im = ax.imshow(logdata, vmin=1, vmax=logdata.max()*0.5)
+        cbar = fig.colorbar(im,ax=ax)
+        plt.title("log image of data after convolved tray has been placed on top")
+        plt.show()
 
     return data
 

ARTIFICIAL/get_artificial_catalog.py
@@ -13,28 +13,35 @@ from scipy.optimize import fsolve
 from numpy import exp, pi, log10
 
 
-def convert_arg_line_to_args(arg_line):
-    for arg in arg_line.split():
-        if not arg.strip():
-            continue
-        yield arg
-
-def getzp(filter):
-    if filter == 'u':
+def getzp(phot_filter):
+    if phot_filter == 'u':
         zp = 0
-    elif filter == 'g':
+    elif phot_filter == 'g':
         zp = 30
-    elif filter == 'r':
+    elif phot_filter == 'r':
         zp = 0
-    elif filter == 'i':
+    elif phot_filter == 'i':
         zp = 0
-    elif filter == 'z':
+    elif phot_filter == 'z':
         zp = 0
     return zp
 
+def getMsun(phot_filter):
+    if phot_filter == 'u':
+        M_sun = 5.49
+    elif phot_filter == 'g':
+        M_sun = 5.23
+    elif phot_filter == 'r':
+        M_sun = 4.53
+    elif phot_filter == 'i':
+        M_sun = 4.14
+    elif phot_filter == 'z':
+        M_sun = 4.01
+    return M_sun
+
 def restofterms(reffs,ns,b_ns,axisratios):
     # 2 pi Reff^2 e^bn n bn^-2n gamma(2n) q
-    return 2*pi*reffs**2*exp(b_ns)*ns*b_ns**(-2*ns)*gamma(2*ns)*axisratios
+    return 2*pi*reffs**2*ns*exp(b_ns)*b_ns**(-2*ns)*gamma(2*ns)*axisratios
 
 def bndefinition(b_n,n):
     #This is the REGULARIZED incomplete gamma function, hence the 1 (not gamma(2*n)). See scipy documentation
@@ -49,7 +56,6 @@ def find_bns(ns,num_dwarfs,verbose):
     if verbose:
         print("finding b_ns")
         t1 = time.perf_counter()
-    #can i not do this all at once
     for i in range(num_dwarfs):
         b_ns[i] = get_the_bn(ns[i])
     if verbose:
@@ -59,15 +65,21 @@ def find_bns(ns,num_dwarfs,verbose):
 
 def find_Ieffs(mags,reffs,ns,b_ns,axisratios,thetas,num_dwarfs,zp,verbose):
     F_tots = 10**(-0.4*(mags-zp))
+    #L_tots = 10**(-0.4*(mags-27.88-M_sun))*L_sun
     Ieffs = F_tots/restofterms(reffs,ns,b_ns,axisratios)
+    #mueffs = mags+5*log10(reffs)+2.5*log10(2*pi*exp(b_ns)*ns*b_ns**(-2*ns)*gamma(2*ns)*axisratios)
+    #print(mueffs)
+    #munought=25.11
+    #Ieffs = 10**(-0.4*(mueffs-munought))
+    #print(Ieffs)
     return Ieffs
 
 def r999definition(r999,b_n,n,reff):
-    return 0.999*gamma(2*n) - gammainc(2*n,b_n*(r999/reff)**1/n)
+    #This is the REGULARIZED incomplete gamma function, hence the 0.999 (not 0.999*gamma(2*n)). See scipy documentation
+    return 0.999 - gammainc(2*n,b_n*(r999/reff)**1/n)
 
 def get_the_r999(b_n,n,reff):
-    root = fsolve(r999definition,x0=[3*reff],args=(b_n,n,reff), full_output=True)
-    print(root)
+    root = fsolve(r999definition,x0=[3*reff],args=(b_n,n,reff))
     return root[0]
 
 def find_r999s(b_ns,ns,reffs):
@@ -75,7 +87,6 @@ def find_r999s(b_ns,ns,reffs):
     if verbose:
         print("finding r999s")
         t1 = time.perf_counter()
-    #can i not do this all at once
     for i in range(num_dwarfs):
         r999s[i] = get_the_r999(b_ns[i],ns[i],reffs[i])
     if verbose:
@@ -83,7 +94,7 @@ def find_r999s(b_ns,ns,reffs):
         print(f"finding r999s time: {t2-t1}")
     return r999s
 
-def clean(filledimage_loc, signature, verbose):
+def clean_up_files(filledimage_loc, signature, verbose):
     if verbose:
         print("cleaning up unneeded files...")
         t1 = time.perf_counter()
@@ -92,7 +103,7 @@ def clean(filledimage_loc, signature, verbose):
         t2 = time.perf_counter()
         print(f"cleaning time: {t2-t1}")
 
-def get_artificial_catalog(data, filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, windowsize, positions, clean, verbose, topdir, signature):
+def get_artificial_catalog(data, phot_filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, windowsize, positions, clean, diagnostic_images, verbose, topdir, signature):
     
     #get randomized parameters for dwarfs
     mags = uniform(mag_range[0],mag_range[1],size=num_dwarfs)
@@ -127,8 +138,10 @@ def get_artificial_catalog(data, filter, mag_range, reff_range, n_range, axisrat
     xs = np.round(x0s/windowsize).astype(int)
     ys = np.round(y0s/windowsize).astype(int)
 
-    #get filter zp based on user input, needed for determining some parameters
-    zp = getzp(filter)
+    #get zeropoint and solar luminosity based on the user-provided phot_filter
+    zp = getzp(phot_filter)
+    M_sun = getMsun(phot_filter)
+    L_sun = 3.828E26
     #also the "/pix for DECam
     res = 0.2637
 
@@ -150,8 +163,8 @@ def get_artificial_catalog(data, filter, mag_range, reff_range, n_range, axisrat
     #data1 = np.copy(data)
     #data2 = np.copy(data)
 
-    data_filled = create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,y0s,num_dwarfs,r999s,psfkernel,True,verbose)
-    #data_filled2 = create_convolve_dwarfs(data2,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,y0s,num_dwarfs,r999s,psfkernel,False,verbose)
+    data_filled = create_convolve_dwarfs(data,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,y0s,num_dwarfs,r999s,psfkernel,True,diagnostic_images,verbose)
+    #data_filled2 = create_convolve_dwarfs(data2,data_shape,Ieffs,reffs,ns,axisratios,thetas,x0s,y0s,num_dwarfs,r999s,psfkernel,False,diagnostic_images,verbose)
     
     outdir = Path(topdir/'OUTPUT'/signature)
     outdir.mkdir(parents=True,exist_ok=True)
@@ -161,14 +174,14 @@ def get_artificial_catalog(data, filter, mag_range, reff_range, n_range, axisrat
     artificial_catalog = np.array((x0s,y0s,mags,Ieff_SBs,I0_SBs,reffs*0.2637,ns,axisratios,thetas,xs,ys)).T
     np.savetxt(outdir/f'{signature}_artificial_dwarfs.catalog',artificial_catalog,fmt=['%-20d','%-20d','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f'],header=f"{'x0':<20s}{'y0':<20s}{'mag':<21s}{'Ieff_SB':<21s}{'I0_SB':<21s}{'reff':<21s}{'n':<21s}{'axisratio':<21s}{'theta':<21s}{'x':<21s}{'y':<21s}")
     if clean:
-        clean(outdir, signature, verbose)
+        clean_up_files(outdir, signature, verbose)
     if verbose:
         print("finished creating artificial image and catalog")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('data', help='Path of the science image containing the real dwarf galaxies. This will be filled with artificial galaxies.')
-    parser.add_argument('filter', choices=['u','g','r','i','z'], help='The photometric filter the data was taken in. This is needed to apply the correct zero point magnitude.')
+    parser.add_argument('phot_filter', choices=['u','g','r','i','z'], help='The photometric filter the data was taken in. This is needed to apply the correct zero point magnitude.')
     parser.add_argument('mag_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the apparent magnitude of the artificial dwarfs.')
     parser.add_argument('reff_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the effective radius of the artificial dwarfs. Provide these numbers in arcseconds. reff, the effective radius, is the radial distance inside of which half of the light of the dwarf is contained. reff and n are used to calculate the other two Sersic parameters, I0 and bn.')
     parser.add_argument('n_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the Sersic index n of the artificial dwarfs. Lower values of n correspond to profiles where the light is more centrally concentrated. reff and n are used to calculate the other two Sersic parameters, I0 and bn.')
@@ -179,11 +192,12 @@ if __name__ == '__main__':
     parser.add_argument('windowsize', type=int, help='The windowsize parameter that will be later used in the detection algorithm. Knowing this allows the program to calculate the coordinates of the artificial dwarfs in the binned image, which is important, since the presence of a detected dwarf at these coordinates indicates a successful detection.')
     parser.add_argument('-positions', nargs='*', help='Optional argument that allows you to specify the coordinates of the dwarfs (i.e., positions are non random). List arguments in the format -positions x y x y ...')
     parser.add_argument('--clean', action='store_true', default=False, help='Deletes output images and files (except, of course, the catalog) after the program has completed. This is useful for saving memory if you do not need to look at the files afterwards.')
+    parser.add_argument('--diagnostic_images', action='store_true', default=False, help='Displays diagnostic images from time to time. These images can be useful but interrupt the program and require the user to not be AFK. They also reduce the speed of the program somewhat.')
     parser.add_argument('--verbose', action='store_true', default=False, help='Displays messages in the terminal.')
 
     args = parser.parse_args()
     data = Path(args.data).resolve()
-    filter = args.filter
+    phot_filter = args.phot_filter
     mag_range = args.mag_range
     #convert to pixels for calculations using the resolution of 0.2637"/pix for DECam
     reff_range = [i/0.2637 for i in args.reff_range]
@@ -195,6 +209,7 @@ if __name__ == '__main__':
     windowsize = args.windowsize
     positions = args.positions
     clean = args.clean
+    diagnostic_images = args.diagnostic_images
     verbose = args.verbose
 
     topdir = Path.cwd().parent
@@ -202,5 +217,5 @@ if __name__ == '__main__':
     filenamestr = data.name.split('.')[0]
     signature = filenamestr+timestr
 
-    get_artificial_catalog(data, filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, windowsize, positions, clean, verbose, topdir, signature)
+    get_artificial_catalog(data, phot_filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, windowsize, positions, clean, diagnostic_images, verbose, topdir, signature)
 

COMPLETION/completion_analysis.py
@@ -1,32 +1,15 @@
-from DETECTION.python.detection_algorithm import detection_algorithm
-from ARTIFICIAL.insert_dwarfs import insert_dwarfs
-from COMPLETION.create_match_catalog import create_match_catalog
-from COMPLETION.combine_catalogs import combine_catalogs
-from COMPLETION.produce_completion_plot import produce_completion_plot
-import os
-import time
 import argparse
 from pathlib import Path
 from datetime import datetime
-#from ddd.combine_catalogues import combine_catalogues
-#from ddd.completion import completion
+from DETECTION import get_detection_catalog
+from ARTIFICIAL import get_artificial_catalog
+from completion.create_match_catalog import create_match_catalog
+from completion.combine_catalogs import combine_catalogs
+from completion.make_completion_plot import produce_completion_plot
 
-def clean(outdir, verbose):
-    if verbose:
-        print("cleaning up unneeded images...")
-        t1 = time.perf_counter()
-    os.remove(outdir/'data_filled.fits')
-    os.remove(outdir/'segment_objs.fits')
-    os.remove(outdir/'masked.fits')
-    os.remove(outdir/'binned.fits')
-    os.remove(outdir/'segment_dets.fits')
-    if verbose:
-        t2 = time.perf_counter()
-        print(f"cleaning time: {t2-t1}")
+def completion_analysis(data, weight, phot_filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, obj_params, maxdilations, maskfunc, windowsize, dolog, det_params, sigclip, num_runs, clean, diagnostic_images, verbose, topdir, signature):
 
-def main(data, weight, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, num_runs, clean, verbose, plots):
-
-    mag_range = [mag_range[0], mag_range[1]]
+    '''mag_range = [mag_range[0], mag_range[1]]
     reff_range = [reff_range[0], reff_range[1]]
     n_range = [n_range[0], n_range[1]]
     axisratio_range = [axisratio_range[0], axisratio_range[1]]
@@ -44,19 +27,23 @@ def main(data, weight, mag_range, reff_range, n_range, axisratio_range, theta_ra
     dw_thresh = 0.5
     dolog = 1
     sig_beneath = 1.05
-    sig_right = 2
+    sig_right = 2'''
 
-    filenamestr = data.name.split('.')[0]
-    timestr = datetime.now().strftime("-%Y%m%d%H%M%S")
+    '''outdir = Path(topdir/'OUTPUT'/signature)
+    outdir.mkdir(parents=True,exist_ok=True)
+    fits.writeto(outdir/f'{signature}_filled.fits',data_filled,header,overwrite=True)
+    #fits.writeto(outdir/f'{signature}_filled2.fits',data_filled2,header,overwrite=True)
+    #if we want reffs in arseconds in the catalog, we convert back using the inverse resolution
+    artificial_catalog = np.array((x0s,y0s,mags,Ieff_SBs,I0_SBs,reffs*0.2637,ns,axisratios,thetas,xs,ys)).T
+    np.savetxt(outdir/f'{signature}_artificial_dwarfs.catalog',artificial_catalog,fmt=['%-20d','%-20d','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f','%-20.5f'],header=f"{'x0':<20s}{'y0':<20s}{'mag':<21s}{'Ieff_SB':<21s}{'I0_SB':<21s}{'reff':<21s}{'n':<21s}{'axisratio':<21s}{'theta':<21s}{'x':<21s}{'y':<21s}")
+    '''
 
     for run_no in range(num_runs):
-        outdir = Path(f"~/Desktop/dwarf_detection/OUTPUT/{filenamestr+timestr}/mag{str(mag_range)}/reff{str(reff_range)}/{filenamestr+timestr+'-'+run_no}")
-        outdir.mkdir(parents=True)
-
-        insert_dwarfs(data, windowsize, num_dwarfs, mag_range, reff_range, n_range, axisratio_range, theta_range, verbose, outdir)
-        detection_algorithm(outdir/'data_filled.fits',weight,maxdilation,windowsize,obj_minarea,obj_thresh,maskfunc,dw_minarea,dw_thresh,dolog,sig_beneath,sig_right,verbose)
-        if clean:
-            clean(outdir,verbose)
+        '''outdir = Path(f"~/Desktop/dwarf_detection/OUTPUT/{filenamestr+timestr}/mag{str(mag_range)}/reff{str(reff_range)}/{filenamestr+timestr+'-'+run_no}")
+        outdir.mkdir(parents=True)'''
+        get_artificial_catalog(data, phot_filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, windowsize, None, clean, diagnostic_images, verbose, topdir, signature)
+        get_detection_catalog(data, weight, obj_params, maxdilations, maskfunc, windowsize, dolog, det_params, sigclip, clean, diagnostic_images, verbose, topdir, signature)
+        
         create_match_catalog(outdir/'artificial_dwarfs.catalog',outdir/'filtered_detections.catalog',outdir/'matches.catalog',verbose)
     combine_catalogs(outdir.parent,num_runs,verbose)
     produce_completion_plot(outdir)
@@ -64,32 +51,54 @@ def main(data, weight, mag_range, reff_range, n_range, axisratio_range, theta_ra
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('data', help='Path of the science image containing the real dwarf galaxies.')
-    parser.add_argument('weight', help='Path of the weight image associated with the data image, used in SExtractor to help with detections.')
+    parser.add_argument('data', help='Path of the data image, including the .fits extension.')
+    parser.add_argument('weight', help='Path of the weight image, including the .fits extension.')
+    parser.add_argument('phot_filter', choices=['u','g','r','i','z'], help='The photometric filter the data was taken in. This is needed to apply the correct zero point magnitude.')
     parser.add_argument('mag_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the apparent magnitude of the artificial dwarfs.')
-    parser.add_argument('reff_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the effective radius of the artificial dwarfs. reff, the effective radius, is the radial distance inside of which half of the light of the dwarf is contained. reff and n are used to calculate the other two Sersic parameters, I0 and bn.')
+    parser.add_argument('reff_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the effective radius of the artificial dwarfs. Provide these numbers in arcseconds. reff, the effective radius, is the radial distance inside of which half of the light of the dwarf is contained. reff and n are used to calculate the other two Sersic parameters, I0 and bn.')
     parser.add_argument('n_range', nargs=2, type=float, help='Two numbers (low and high) that specify the range of the Sersic index n of the artificial dwarfs. Lower values of n correspond to profiles where the light is more centrally concentrated. reff and n are used to calculate the other two Sersic parameters, I0 and bn.')
     parser.add_argument('axisratio_range', nargs=2, type=float, help='Two numbers (low and high) that specify the axis ratio range of the artificial dwarfs. The axis ratio takes a value from 0 to 1, where 0 is unphysical and 1 is perfectly circular. (It is the complement of the ellipticity)')
     parser.add_argument('theta_range', nargs=2, type=float, help='Two numbers (low and high) that specify the angular offset range of artificial dwarfs, in degrees. Enter 0 and 360 if you want to include all possible angles.')
-    parser.add_argument('num_dwarfs', type=int, help='number of dwarfs')
-    parser.add_argument('num_runs', type=int, help='number of runs')
-    parser.add_argument('-clean', action='store_true', default=False, help='Whether at the end of each run to clean up the images that are used by the program. If you want to keep and inspect these images, do not provide this argument. Note however that several images are produced each run with about the same size as the original data image you input, and so, if you are using a large image, not providing this argument may cause a large amount of disk space to be consumed, especially if you are doing more than a couple of runs.')
-    parser.add_argument('-verbose', action='store_true', default=False, help='Whether to display messages on the console as the program works. These messages indicate what task the program is currently working on as well as how long the last task took.')
-    parser.add_argument('-plots', action='store_true', default=False, help='Whether to display plots at certain points. These plots are useful for seeing what the program is doing, but displaying these plots comes with a downside; namely, that the execution of the program is halted whenever a plot is shown. The user must manually close the window containing the plot in order for the program to resume. This is undesirable if the user simply wants to let the program run for a long time in an unsupervised manner.')
+    parser.add_argument('num_dwarfs', type=int, help='The number of artificial dwarfs to insert into the data image.')
+    parser.add_argument('psf', help='Path to the psf used to convolve the artificial dwarfs.')
+    parser.add_argument('-obj_params', nargs=2, default=[10,30], help='Enter two numbers for the DETECT_MINAREA and DETECT_THRESH sextractor parameters used to generate the segmentation image which later gets turned into a mask.')
+    parser.add_argument('-maxdilations', default=10, type=int, help='Maximum number of times to binary dilate the objects in the sextractor-output segmentation image, with each of the diamond and square kernels. In other words, the largest objects will be dialated (twice) this many times. The number of dialations an object undergoes depends on its size, using the function specified.')
+    parser.add_argument('-maskfunc', type=str, choices=['sinh','expsq'], default='expsq', help='Function used to calculate the mask size distribution according to object size. Choices are sinh and expsq. sinh avoids masking smaller objects; expsq with too high a maxdilation may cause masking of dwarfs. Default is expsq.')
+    parser.add_argument('-windowsize', default=10, type=int, help='Size of the window used to bin the pixels in the masked image. The image is divided into square windows having the specified sidelength. The median of the pixels within each window is written to a single pixel in the output image.')
+    parser.add_argument('-dolog', type=int, default=1, help='Whether or not the binned image is log normalized. Specify 1 for True or 0 for False. Default is 1.')
+    parser.add_argument('-det_params', nargs=2, default=[5,1], help='Enter two numbers for the DETECT_MINAREA and DETECT_THRESH sextractor parameters used when performing detection on the binned image to obtain the raw (unfiltered) detections.')
+    parser.add_argument('-sigclip', nargs=2, type=float, default=[1.05,2.5], help='Number of standard deviations the kappa sigma clippers use to filter the raw detections. The first argument is the number of standard devations beneath the fitted exponential (in MAG_AUTO vs. FLUX_RAD). The second refers to the number of standard deviations to the right of the mean flux value.')
+    parser.add_argument('num_runs', type=int, help='The number of times to construct artificial and detection catalogs using the same ranges of artificial dwarf parameters.')
+    parser.add_argument('--clean', action='store_true', default=False, help='Deletes output images and files (except, of course, the catalog) after the program has completed. This is useful for saving memory if you do not need to look at the files afterwards.')
+    parser.add_argument('--diagnostic_images', action='store_true', default=False, help='Displays diagnostic images from time to time. These images can be useful but interrupt the program and require the user to not be AFK. They also reduce the speed of the program somewhat.')
+    parser.add_argument('--verbose', action='store_true', default=False, help='Displays messages in the terminal.')
 
     args = parser.parse_args()
     data = Path(args.data).resolve()
     weight = Path(args.weight).resolve()
-    weight = args.weight
+    phot_filter = args.phot_filter
     mag_range = args.mag_range
     reff_range = args.reff_range
     n_range = args.n_range
     axisratio_range = args.axisratio_range
     theta_range = args.theta_range
     num_dwarfs = args.num_dwarfs
+    psf = args.psf
+    obj_params = args.obj_params
+    maxdilations = args.maxdilations
+    maskfunc = args.maskfunc
+    windowsize = args.windowsize
+    dolog = bool(args.dolog)
+    det_params = args.det_params
+    sigclip = args.sigclip
     num_runs = args.num_runs
     clean = args.clean
+    diagnostic_images = args.diagnostic_images
     verbose = args.verbose
-    plots = args.plots
 
-    main(data,weight,mag_range,reff_range,n_range,axisratio_range,theta_range,num_dwarfs,num_runs,clean,verbose,plots)
+    topdir = Path.cwd().parent
+    timestr = datetime.now().strftime("-%Y%m%d%H%M%S")
+    filenamestr = data.name.split('.')[0]
+    signature = filenamestr+timestr
+
+    completion_analysis(data, weight, phot_filter, mag_range, reff_range, n_range, axisratio_range, theta_range, num_dwarfs, psf, obj_params, maxdilations, maskfunc, windowsize, dolog, det_params, sigclip, num_runs, clean, diagnostic_images, verbose, topdir, signature)